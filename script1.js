824f1398ced6bf69498b08f68512af56ddf69bbf
mouhsineelhlaissi <mouhsinesm@gmail.com>
2021-08-31 12:26:02 +0100

    hh

---
+++ script1.js
@@ -0,0 +1,12 @@
+var leiba=document.getElementById("twichia")
+var menu=document.getElementById("bt");
+menu.addEventListener("click",fct);
+ function fct (){
+
+    console.log(leiba.value);
+}
+
+var test ="hello world"
+test1 =test.length[5];
+console.log(test1);
+
