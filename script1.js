54959317d0b794021e0433abfef65359772f92e0
mouhsineelhlaissi <mouhsinesm@gmail.com>
2021-09-01 20:48:57 +0100

    if exercice

--- script1.js
+++ script1.js
@@ -4,7 +4,15 @@ menu.addEventListener("click",fct);
  function fct (){
 
 var test =leiba.value;
-var test1 =test.length
-console.log(test[test1/2]);
+var test1 =test.length;
+console.log(test1);
+if(test1%2==0){
+    
+    console.log(test[(test1-2)/2]);
+    
+} else {
+    console.log(test[(test1-1)/2]);  
+}
+
  }
 
