54b0d67526026ca0a464c8aa83370cba2e3c451e
mouhsineelhlaissi <mouhsinesm@gmail.com>
2021-09-01 19:50:21 +0100

    exercice3

--- script1.js
+++ script1.js
@@ -3,8 +3,8 @@ var menu=document.getElementById("bt");
 menu.addEventListener("click",fct);
  function fct (){
 
-var test =leiba.value.toString;
-vat test2 =test.length[1];
-console.log(test2);
+var test =leiba.value;
+var test1 =test.length
+console.log(test[test1/2]);
  }
 
