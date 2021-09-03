c3957c579b931b6051c86fd402b8822f1468ed45
mouhsineelhlaissi <mouhsinesm@gmail.com>
2021-09-03 22:18:09 +0100

    the half of text

--- script1.js
+++ script1.js
@@ -3,17 +3,16 @@ var menu=document.getElementById("bt");
 menu.addEventListener("click",fct);
  function fct (){
 
-var test =leiba.value;
+var test =leiba.value
 var test1 =test.length;
-console.log("the first letter",test[0]);
-console.log("the final letter",test[test1-1]);
 if(test1%2==0){
-    
-    console.log("the middle letter",test[(test1-2)/2]);
-    
-} else {
-    console.log("the middle letter",test[(test1-1)/2]) 
+for(let i = 0; i < test1/2 ; i++){
+console.log(test[i])
 }
+}else{
+    for(let i = 0; i < (test1+1)/2 ; i++){
+        console.log(test[i])
 
+}
+}
  }
-
