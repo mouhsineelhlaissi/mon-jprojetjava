55f643e40db24cdbbd467f9fe66594f1e406ddae
mouhsineelhlaissi <mouhsinesm@gmail.com>
2021-09-03 12:39:41 +0100

    the final exercive

--- script1.js
+++ script1.js
@@ -5,13 +5,14 @@ menu.addEventListener("click",fct);
 
 var test =leiba.value;
 var test1 =test.length;
-console.log(test1);
+console.log("the first letter",test[0]);
+console.log("the final letter",test[test1-1]);
 if(test1%2==0){
     
-    console.log(test[(test1-2)/2]);
+    console.log("the middle letter",test[(test1-2)/2]);
     
 } else {
-    console.log(test[(test1-1)/2])
+    console.log("the middle letter",test[(test1-1)/2]) 
 }
 
  }
